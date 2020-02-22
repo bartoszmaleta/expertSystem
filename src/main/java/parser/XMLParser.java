c092c62a3bbe0618bd79eebe61d0280273cf139a
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-22 21:30:33 +0100

    Add xmls + XMLParser

--- src/main/java/parser/XMLParser.java
+++ src/main/java/parser/XMLParser.java
@@ -1,4 +1,35 @@
 package parser;
 
-public class XMLParser {
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public abstract class XMLParser {
+    Document doc;
+
+    public void loadXmlDocument(String xmlPath) {
+        try {
+            File xmlFile = new File(xmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist");
+        } catch (IOException e) {
+            System.out.println("Error during reading");
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
 }
