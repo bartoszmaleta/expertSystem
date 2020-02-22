c0e4464741568c2c16851320f502efaec614350d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-22 21:02:01 +0100

    Add FactRepository

---
+++ src/main/java/facts/FactRepository.java
@@ -0,0 +1,48 @@
+package facts;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class FactRepository {
+    private List<Fact> facts;
+    private int size;
+
+    public FactRepository(List<Fact> facts) {
+        this.facts = facts;
+    }
+
+    public List<Fact> getFacts() {
+        return facts;
+    }
+
+    public void addFact(Fact fact) {
+        facts.add(fact);
+    }
+
+    private class FactIterator implements Iterator {
+        int index;
+
+
+        @Override
+        public boolean hasNext() {
+            if (index < facts.size()) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Fact next() {
+            if (this.hasNext()) {
+                return facts.get(index++);
+            }
+            return null;
+        }
+
+        // TODO: remove?
+    }
+
+    public Iterator getIterator() {
+        return new FactIterator();
+    }
+}
