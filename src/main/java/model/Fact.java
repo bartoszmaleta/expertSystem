9c7f4344ce4760a64f307f59a4710b5c5b346bb7
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-22 21:46:12 +0100

    Change Fact

--- src/main/java/model/Fact.java
+++ src/main/java/model/Fact.java
@@ -1,30 +1,38 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Fact {
     private String id;
     private Set<String> evals;
     private String description;
+    private Map<String, Boolean> properties;
 
-    public Fact(String id, Set<String> evals, String description) {
+    public Fact(String id,  String description) {
         this.id = id;
-        this.evals = evals;
         this.description = description;
+        properties = new HashMap<>();
     }
 
     public void addEval(String eval) {
         this.evals.add(eval);
     }
 
-    public Set<String> getEvals() {
-        return evals;
+    public Map<String, Boolean> getProperties() {
+        return  properties;
     }
 
     public void setFactValueById(String id, boolean value) {
 //        TODO
     }
 
+    public Set<String> getEvals() {
+        return evals;
+    }
+
+    // TODO
     public boolean getValueById(String id) {
 //        int idInt = Integer.parseInt(id);
         for (String eval : evals) {
--- src/main/java/parser/FactParser.java
+++ src/main/java/parser/FactParser.java
@@ -1,6 +1,10 @@
 package parser;
 
+import model.Fact;
 import model.FactRepository;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class FactParser extends XMLParser {
     private FactRepository factRepository;
@@ -13,6 +17,23 @@ public class FactParser extends XMLParser {
 
 
     private void parse() {
+        NodeList factList = doc.getElementsByTagName("Fact");
+        for (int i = 0; i < factList.getLength(); i++) {
+            Node node = factList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+
+                String factID = element.getAttribute("id");
+                String factDescription = element.getChildNodes()
+                        .item(1)
+                        .getAttributes()
+                        .item(0)
+                        .getTextContent();
+
+                Fact newFact = new Fact(factID, factDescription);
+
+            }
+        }
         // TODO
     }
 
