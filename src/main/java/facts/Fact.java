7e6f7cdf832b7fb468d09a7923670caf7b11c3f1
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-22 20:51:51 +0100

    Add Fact

---
+++ src/main/java/facts/Fact.java
@@ -0,0 +1,41 @@
+package facts;
+
+import java.util.Set;
+
+public class Fact {
+    private String id;
+    private Set<String> evals;
+    private String description;
+
+    public Fact(String id, Set<String> evals, String description) {
+        this.id = id;
+        this.evals = evals;
+        this.description = description;
+    }
+
+    public void addEval(String eval) {
+        this.evals.add(eval);
+    }
+
+    public Set<String> getEvals() {
+        return evals;
+    }
+
+    public void setFactValueById(String id, boolean value) {
+//        TODO
+    }
+
+    public boolean getValueById(String id) {
+//        int idInt = Integer.parseInt(id);
+        for (String eval : evals) {
+            if (eval.equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
