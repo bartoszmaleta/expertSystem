26f682be3698e2a287f13ecaebb0f18c1d551c5d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-23 16:10:16 +0100

    Add instances + constructor + addQuestion + getIterator + implement QuestionIterator

--- src/main/java/repository/RuleRepository.java
+++ src/main/java/repository/RuleRepository.java
@@ -1,7 +1,45 @@
 package repository;
 
+import model.Question;
+
+import java.util.*;
+
 public class RuleRepository {
     //        TODO
+    private Iterator<Question> questionIterator;
+    private Map<String, Question> questions;
+
+    public RuleRepository() {
+        questionIterator = new QuestionIterator();
+        questions = new HashMap<>();
+    }
+
+    public void addQuestion(Question question) {
+        questions.put(question.getId(), question);
+    }
+
+    public Iterator<Question> getIterator() {
+        return questionIterator;
+    }
+
+    private class QuestionIterator implements Iterator<Question> {
+        int index = 0;
+        List<String> keyList;
 
 
+        @Override
+        public boolean hasNext() {
+            return index < questions.size();
+        }
+
+        @Override
+        public Question next() {
+            if (keyList == null) {
+                keyList = new ArrayList<>(questions.keySet());
+            }
+
+            String key = keyList.get(index++);
+            return questions.get(key);
+        }
+    }
 }
