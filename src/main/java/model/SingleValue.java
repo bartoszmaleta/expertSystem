473b0d0501f8ec7f434540c88c7f14a6ea34f46e
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-23 15:51:14 +0100

    Add construcotr + override getInputPattern()

--- src/main/java/model/SingleValue.java
+++ src/main/java/model/SingleValue.java
@@ -1,9 +1,20 @@
 package model;
 
-import com.sun.jdi.Value;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SingleValue extends Value {
     private String param;
 
+    public SingleValue(String param, boolean selectionType) {
+        this.param = param;
+        this.selectionType = selectionType;
+    }
 
+    @Override
+    public List<String> getInputPattern() {
+        List<String> paramList = new ArrayList<>();
+        paramList.add(param);
+        return  paramList;
+    }
 }
