b59fbdd4eda5ac9f5cd562ea7197364caff881ff
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-02-23 16:01:02 +0100

    Add instances + constructor + getters + getEvaluatedAnswer()

---
+++ src/main/java/model/Question.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.InputMismatchException;
+
+public class Question {
+    private String id;
+    private String question;
+    private Answer answer;
+
+    public Question(String id, String question, Answer answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public boolean getEvaluatedAnswer(String input) throws InputMismatchException {
+        return answer.evaluateAnswerByInput(input);
+    }
+}
